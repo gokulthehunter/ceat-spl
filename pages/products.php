1828f4ebbe82e3e52b8b059f08af70b358f9bd23
gokul <gokul@22feettribalww.com>
2019-04-06 18:52:46 +0530

    product landing added

---
+++ pages/products.php
@@ -0,0 +1,199 @@
+<?php include("../includes/settings.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+    <?php include('../includes/header-imports.php') ?>
+</head>
+<body>
+    <!-- common loader[start] -->
+    <?php include('../components/shared/loader.php') ?>
+    <!-- common loader[end] -->
+    <!-- website header[start] -->
+    <?php include('../components/shared/header.php') ?> 
+    <!-- website header[end] -->
+    <main>
+        <!-- cremb and heading[start] -->
+        <div class="page_head">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page_head_cremb">
+                            <a href="javascript:void(0)" class="page_head_cremb--link">Home</a>
+                            <a class="page_head_cremb--link">Products</a>
+                        </div>
+                        <h1 class="page_head--title">Products</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- cremb and heading[end] -->
+        <section class="prod_posts py-5">
+            <div class="container">
+                <div class="row">
+                    <!-- 1 -->
+                    <div class="col-12 py-5">
+                        <div class="prod_posts_each">
+                            <div class="row align-items-center">
+                                <div class="col-md-7">
+                                    <div class="c_title">
+                                        <h2 class="c_title--inner lined">Agriculture</h2>
+                                    </div>
+                                    <div class="part-top">
+                                        <div class="py-3"></div>
+                                        <p>The Radial Range</p>
+                                        <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Radial Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+
+                                    <div class="part-bottom">
+                                        <div class="py-3"></div>
+                                        <p>The Radial Range</p>
+                                        <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Bias Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <img src="<?php echo  $imagesurl ?>prod-use-1.jpg" class="img-fluid" alt="image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- 2 -->
+                    <div class="col-12 py-5">
+                        <div class="prod_posts_each">
+                            <div class="row align-items-center r_xs--rev">
+                                <div class="col-md-7">
+                                    <div class="c_title">
+                                        <h2 class="c_title--inner lined">Port & Industrial</h2>
+                                    </div>
+                                    <div class="part-top">
+                                        <div class="py-3"></div>
+                                        <p>For Industrial and Port applications, CEAT Specialty tyres come with specially designed tread patterns, superior compound formulations and strong nylon casings. This ensures optimum treadwear, traction and trouble-free long service life. Choose from our variety of sizes and tread designs for Port and Industrial application. The offering includes tyres for Industrial Forklift, reach stacker, container handler, skid steer, multi-purpose trucks.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <img src="<?php echo  $imagesurl ?>prod-use-2.jpg" class="img-fluid" alt="image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- 3 -->
+                    <div class="col-12 py-5">
+                        <div class="prod_posts_each">
+                            <div class="row align-items-center">
+                                <div class="col-md-7">
+                                    <div class="c_title">
+                                        <h2 class="c_title--inner lined">Agriculture</h2>
+                                    </div>
+                                    <div class="part-top">
+                                        <div class="py-3"></div>
+                                        <p>The Radial Range</p>
+                                        <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Radial Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+
+                                    <div class="part-bottom">
+                                        <div class="py-3"></div>
+                                        <p>The Radial Range</p>
+                                        <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Bias Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <img src="<?php echo  $imagesurl ?>prod-use-1.jpg" class="img-fluid" alt="image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- 4 -->
+                    <div class="col-12 py-5">
+                        <div class="prod_posts_each">
+                            <div class="row align-items-center r_xs--rev">
+                                <div class="col-md-7">
+                                    <div class="c_title">
+                                        <h2 class="c_title--inner lined">Port & Industrial</h2>
+                                    </div>
+                                    <div class="part-top">
+                                        <div class="py-3"></div>
+                                        <p>For Industrial and Port applications, CEAT Specialty tyres come with specially designed tread patterns, superior compound formulations and strong nylon casings. This ensures optimum treadwear, traction and trouble-free long service life. Choose from our variety of sizes and tread designs for Port and Industrial application. The offering includes tyres for Industrial Forklift, reach stacker, container handler, skid steer, multi-purpose trucks.</p>
+                                        <div class="py-2"></div>
+                                        <a href="javascript:void(0)" class="prod_posts_each--btn">View Tyres
+                                            <svg  width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                                <g transform="translate(38.597 29.637) rotate(180)">
+                                                    <g transform="translate(34.844 22.937)">
+                                                        <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <img src="<?php echo  $imagesurl ?>prod-use-2.jpg" class="img-fluid" alt="image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- website footer[start] -->
+    <?php include('../components/shared/footer.php') ?>
+    <!-- website footer[end] -->
+    <!-- scripts[start] -->
+    <?php include('../includes/bottom-scripts.php') ?> 
+    <!-- scripts[end] -->
+    <!-- page script -->
+</body>
+</html>
