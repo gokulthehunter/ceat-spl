2ecdb308bcf9c9ce10f6d04d370eff4a7d86500e
gokul <gokul@22feettribalww.com>
2019-04-08 23:35:04 +0530

    investors page done

---
+++ pages/investors.php
@@ -0,0 +1,181 @@
+<?php include("../includes/settings.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Investors</title>
+    <?php include('../includes/header-imports.php') ?>
+</head>
+<body>
+    <!-- common loader[start] -->
+    <?php include('../components/shared/loader.php') ?>
+    <!-- common loader[end] -->
+    <!-- website header[start] -->
+    <?php include('../components/shared/header.php') ?> 
+    <!-- website header[end] -->
+    <main>
+        <!-- cremb and heading[start] -->
+        <div class="page_head">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page_head_cremb">
+                            <a href="javascript:void(0)" class="page_head_cremb--link">Home</a>
+                            <a class="page_head_cremb--link">Investors</a>
+                        </div>
+                        <h1 class="page_head--title">Investors</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- cremb and heading[end] -->
+        <!-- investors highlights[start] -->
+        <section class="invest_high">
+            <div class="container py-5">
+                <div class="row align-items-start">
+                    <div class="col-md-5 offset-md-1">
+                        <div class="invest_high_content px-3 py-4">
+                            <div class="c_title">
+                                <h2 class="c_title--inner lined small">Sixth Annual General Meeting</h2>
+                            </div>
+                            <p class="invest_high_content--text">Solutions for all farmers on all continents! SIMA 2019 will be held from 24 to 28 February 2019 at Paris Nord Villepinte, France.</p>
+                            <div class="c_btn">
+                                <a href="javascript:void(0)" class="c_btn--rounded">Download</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="invest_high_content px-3 py-4">
+                            <div class="c_title">
+                                <h2 class="c_title--inner lined small">Sixth Annual General Meeting</h2>
+                            </div>
+                            <p class="invest_high_content--text">Solutions for all farmers on all continents! SIMA 2019 will be held from 24 to 28 February 2019 at Paris Nord Villepinte, France.</p>
+                            <div class="c_btn">
+                                <a href="javascript:void(0)" class="c_btn--rounded">Download</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- investors highlights[end] -->
+        <!-- our policy[start] -->
+        <section class="invest_policy bg_light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 offset-md-1">
+                        <div class="px-3">
+                            <div class="invest_policy_holder">
+                                <div class="c_title">
+                                    <h2 class="c_title--inner lined big">Our Policies</h2>
+                                </div>
+                                <p class="invest_policy_holder--text">Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <a href="javascript:void(0)" class="invest_policy_holder--btn">
+                                    <span>Know More</span>
+                                    <svg width="9.466" height="5.301" viewBox="0 0 9.466 5.301">
+                                        <g transform="translate(-22.938 40.145) rotate(-90)">
+                                            <g transform="translate(34.844 22.937)">
+                                                <path d="M34.844,980.032l.344.361,4.2,4.372.76-.721-3.853-4.011,3.853-4.011-.76-.721-4.2,4.372Z" transform="translate(-34.844 -975.3)"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- our policy[end] -->
+        <!-- board of directors[start] -->
+        <section class="board_dir bg_light py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="c_title text-center pt-4 pb-5">
+                            <h2 class="c_title--inner">Board of Directors</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/1.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">Chairman</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/2.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">CEO</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/1.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">Chairman</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/2.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">CEO</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/1.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">Chairman</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/2.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">CEO</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/1.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">Chairman</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/2.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">CEO</p>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="board_dir_each">
+                            <img src="<?php echo $imagesurl ?>investors/1.jpg" class="img-fluid" alt="investor">
+                            <h5 class="board_dir_each--name">Mr. Anant Goenka</h5>
+                            <p class="board_dir_each--desg">Chairman</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- board of directors[end] -->
+    </main>
+    <!-- website footer[start] -->
+    <?php include('../components/shared/footer.php') ?>
+    <!-- website footer[end] -->
+
+    <!-- additonal popups[start] -->
+    <?php include('../components/video-popup.php') ?>
+    <!-- additonal popups[end] -->
+
+    <!-- scripts[start] -->
+    <?php include('../includes/bottom-scripts.php') ?> 
+    <!-- scripts[end] -->
+    <!-- page script -->
+</body>
+</html>
