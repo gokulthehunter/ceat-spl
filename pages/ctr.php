e1bdcb978d6c4628e834ae9121ec1f01e4106bc4
gokul <gokul@22feettribalww.com>
2019-04-09 19:57:38 +0530

    ctr page done

---
+++ pages/ctr.php
@@ -0,0 +1,178 @@
+<?php include("../includes/settings.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Compaction</title>
+    <?php include('../includes/header-imports.php') ?>
+</head>
+<body>
+    <!-- common loader[start] -->
+    <?php include('../components/shared/loader.php') ?>
+    <!-- common loader[end] -->
+    <!-- website header[start] -->
+    <?php include('../components/shared/header.php') ?> 
+    <!-- website header[end] -->
+    <main>
+        <!-- cremb and heading[start] -->
+        <div class="page_head bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page_head_cremb">
+                            <a href="javascript:void(0)" class="page_head_cremb--link">Home</a>
+                            <a class="page_head_cremb--link">CTR</a>
+                        </div>
+                        <h1 class="page_head--title">Compaction</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- cremb and heading[end] -->
+        <section class="ctr_wrapper">
+            <div class="ctr_wrapper_each">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 order-2 order-md-1">
+                            <div class="ctr_wrapper_each_content">
+                                <p>Larger footprint ensures reduced soil compaction and increases farm productivity. Superior construction allows the tyres to operate with high load carrying capacity on road and at lower inflation pressure onfield. This helps further increase the footprint and reduce soil compaction.</p>
+                                <h4 class="ctr_wrapper_each_content--title">Increased Footprint.<br/>Reduced Soil Compaction.</h4>
+                                <div class="ctr_wrapper_each_content--more mb-2 exp-content">
+                                    <div class="c_title mb-2">
+                                        <h4 class="c_title--inner lined">Low Compaction</h4>
+                                    </div>
+                                    <p>A larger inner volume, higher tread arc width and flexible sidewall combine to make for a larger footprint. This contributes to “Low Compaction” which reduces soil damage. This helps aeration and maintains the proper mechanical strength of the soil, contributing to higher crop yield.. We’re  tough on our tyres, so they can be gentle on your soil and protect your crop.</p>
+                                </div>
+                                <a href="javascript:void(0)" class="ctr_wrapper_each_content--btn exp-btn">
+                                    <span>Read More</span>
+                                    <svg width="9.466" height="5.301" viewBox="0 0 9.466 5.301">
+                                        <g transform="translate(-22.938 40.145) rotate(-90)">
+                                            <g transform="translate(34.844 22.937)">
+                                                <path d="M34.844,980.032l.344.361,4.2,4.372.76-.721-3.853-4.011,3.853-4.011-.76-.721-4.2,4.372Z" transform="translate(-34.844 -975.3)"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 order-1 order-md-2">
+                            <div class="ctr_wrapper_each_poster" id="sticky_item">
+                                <div class="prod_posts_each_video v-pop" data-type="youtube" data-id="udnjPDsUBug" data-autoplay='true'>
+                                    <img src="<?php echo $imagesurl ?>ctr-poster.jpg" class="img-fluid" alt="image">
+                                    <div class="prod_posts_each_video--btn text-center">
+                                        <svg viewBox="0 0 55 55" style="enable-background:new 0 0 55 55;">
+                                            <g>
+                                                <path fill="#076ED5" d="M27.5,0C42.7,0,55,12.3,55,27.5S42.7,55,27.5,55S0,42.7,0,27.5S12.3,0,27.5,0z"/>
+                                                <path fill="#F6FAFB" d="M25.1,20.1v14.4l8.5-7.2L25.1,20.1z"/>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <p class="prod_posts_each_video--text text-center">CTR Animations</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ctr_wrapper_each bg_light">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="c_title mb-2">
+                                <h4 class="c_title--inner">Traction</h4>
+                            </div>
+                            <div class="ctr_wrapper_each_content">
+                                <p>Higher lug overlap gives better stability and more comfort on the road. We have specially designed the lugs for higher continuity, higher overlap at the centre, and optimum angle to provide better riding comfort and less vibrations on the road.</p>
+                                <h4 class="ctr_wrapper_each_content--title">Greater Lug Overlap.<br/>Less Vibrations on Road.</h4>
+                                <div class="ctr_wrapper_each_content--more mb-2 exp-content">
+                                    <div class="c_title mb-2">
+                                        <h4 class="c_title--inner lined">Low Traction</h4>
+                                    </div>
+                                    <p>A larger inner volume, higher tread arc width and flexible sidewall combine to make for a larger footprint. This contributes to “Low Compaction” which reduces soil damage. This helps aeration and maintains the proper mechanical strength of the soil, contributing to higher crop yield.. We’re  tough on our tyres, so they can be gentle on your soil and protect your crop.</p>
+                                </div>
+                                <a href="javascript:void(0)" class="ctr_wrapper_each_content--btn exp-btn">
+                                    <span>Read More</span>
+                                    <svg width="9.466" height="5.301" viewBox="0 0 9.466 5.301">
+                                        <g transform="translate(-22.938 40.145) rotate(-90)">
+                                            <g transform="translate(34.844 22.937)">
+                                                <path d="M34.844,980.032l.344.361,4.2,4.372.76-.721-3.853-4.011,3.853-4.011-.76-.721-4.2,4.372Z" transform="translate(-34.844 -975.3)"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ctr_wrapper_each">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="c_title mb-2">
+                                <h4 class="c_title--inner">Roadability</h4>
+                            </div>
+                            <div class="ctr_wrapper_each_content">
+                                <p>Better grip reduces slippage and lowers fuel consumption. Understanding this, we have designed the tyres with optimum lug angles to provide enhanced traction in mud and slush.</p>
+                                <h4 class="ctr_wrapper_each_content--title">Low Angle Lugs.<br/>High Levels of Grip.</h4>
+                                <div class="ctr_wrapper_each_content--more mb-2 exp-content">
+                                    <div class="c_title mb-2">
+                                        <h4 class="c_title--inner lined">Compaction</h4>
+                                    </div>
+                                    <p>A larger inner volume, higher tread arc width and flexible sidewall combine to make for a larger footprint. This contributes to “Low Compaction” which reduces soil damage. This helps aeration and maintains the proper mechanical strength of the soil, contributing to higher crop yield.. We’re  tough on our tyres, so they can be gentle on your soil and protect your crop.</p>
+                                </div>
+                                <a href="javascript:void(0)" class="ctr_wrapper_each_content--btn exp-btn">
+                                    <span>Read More</span>
+                                    <svg width="9.466" height="5.301" viewBox="0 0 9.466 5.301">
+                                        <g transform="translate(-22.938 40.145) rotate(-90)">
+                                            <g transform="translate(34.844 22.937)">
+                                                <path d="M34.844,980.032l.344.361,4.2,4.372.76-.721-3.853-4.011,3.853-4.011-.76-.721-4.2,4.372Z" transform="translate(-34.844 -975.3)"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- website footer[start] -->
+    <?php include('../components/shared/footer.php') ?>
+    <!-- website footer[end] -->
+    <!-- additonal popups[start] -->
+    <?php include('../components/video-popup.php') ?>
+    <!-- additonal popups[end] -->
+    <!-- scripts[start] -->
+    <?php include('../includes/bottom-scripts.php') ?> 
+    <!-- scripts[end] -->
+    <!-- page dependancy -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sticky-kit/1.1.3/sticky-kit.min.js"></script>
+    <!-- page script -->
+    <script>
+        // loading mock
+        jQuery(document).ready(function($){
+            $(document).on("click",".exp-btn",function(){
+                var _this = $(this),
+                    _actClass = "state-active";
+                _this.prev(".exp-content").slideToggle();
+                _this.toggleClass(_actClass);
+                if(_this.hasClass(_actClass)){
+                    _this.find("span").text("Read Less");
+                }else{
+                    _this.find("span").text("Read More");
+                }
+            });
+
+            if($(window).width() > 767){
+                $(".ctr_wrapper_each_poster").stick_in_parent({
+                    parent: ".ctr_wrapper",
+                    offset_top:150
+                });
+            }
+        }(jQuery));
+        // $(window).on("load",function(){
+        // })
+    </script>
+</body>
+</html>
