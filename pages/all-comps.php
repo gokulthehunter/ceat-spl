4145cd9600cb339a00fb4fde151b71b83fd7ed42
gokul <gokul@22feettribalww.com>
2019-04-13 11:31:54 +0530

    comp updates

---
+++ pages/all-comps.php
@@ -0,0 +1,233 @@
+<?php include("../includes/settings.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page</title>
+    <?php include('../includes/header-imports.php') ?>
+</head>
+<body>
+    <!-- website header[start] -->
+    <?php include('../components/shared/header.php') ?> 
+    <!-- website header[end] -->
+    <!-- common loader[start] -->
+    <?php include('../components/shared/loader.php') ?>
+    <!-- common loader[end] -->
+    <main>
+        <!-- main slider[start] -->   
+        <section class="main_banner">
+            <div class="hero_slider swiper-container">
+                <div class="swiper-wrapper">
+                    <!-- itrator div starts-->
+                    <div class="hero_slider_each swiper-slide"><!-- itrator div starts-->
+                        <div class="hero_slider_each--img" style="background-image: url('<?php echo $imagesurl ?>main-banner.jpg')"></div>                        
+                        <div class="hero_slider_each_data">
+                            <div class="container">
+                                <h2 class="hero_slider_each_data--title">GREATER LUG OVERLAP.<br/>LESS VIBRATIONS ON ROAD.</h2>
+                                <h2 class="hero_slider_each_data--sub">Presenting CEAT Farmax85 Farm Radials</h2>
+                                <a href="javascript:void(0)" class="hero_slider_each_data--btn">Know More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero_slider_each swiper-slide">
+                        <div class="hero_slider_each--img" style="background-image: url('<?php echo $imagesurl ?>main-banner.jpg')"></div>
+                        <div class="hero_slider_each_data">
+                            <div class="container">
+                                <h2 class="hero_slider_each_data--title">GREATER LUG OVERLAP.<br/>LESS VIBRATIONS ON ROAD.</h2>
+                                <h2 class="hero_slider_each_data--sub">Presenting CEAT Farmax85 Farm Radials</h2>
+                                <a href="javascript:void(0)" class="hero_slider_each_data--btn">Know More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero_slider_each swiper-slide"><!-- end div starts-->
+                        <div class="hero_slider_each--img" style="background-image: url('<?php echo $imagesurl ?>main-banner.jpg')"></div>
+                        <div class="hero_slider_each_data">
+                            <div class="container">
+                                <h2 class="hero_slider_each_data--title">GREATER LUG OVERLAP.<br/>LESS VIBRATIONS ON ROAD.</h2>
+                                <h2 class="hero_slider_each_data--sub">Presenting CEAT Farmax85 Farm Radials</h2>
+                                <a href="javascript:void(0)" class="hero_slider_each_data--btn">Know More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider_controls">
+                    <div class="slider_controls_arrows slide-prev">
+                        <svg width="12.297" height="10.078" viewBox="0 0 12.297 10.078">
+                            <g transform="translate(-0.591 11.664) rotate(-90)">
+                                <g transform="translate(1.586 0.591)">
+                                    <path d="M7.109,12.888H6.141V2.443L2.27,6.314,1.586,5.63,6.625.591,11.664,5.63l-.684.684L7.109,2.443Z" transform="translate(-1.586 -0.591)" fill-rule="evenodd"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="slider_controls_dots slide-dots"></div>
+                    <div class="slider_controls_arrows slide-next">
+                        <svg width="12.298" height="10.078" viewBox="0 0 12.298 10.078">
+                            <g  transform="translate(12.888 -1.586) rotate(90)">
+                                <g transform="translate(1.586 0.591)">
+                                    <path d="M7.109,12.888H6.141V2.443L2.27,6.314,1.586,5.63,6.625.591,11.664,5.63l-.684.684L7.109,2.443Z" transform="translate(-1.586 -0.591)" fill="#076ed5" fill-rule="evenodd"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </section>   
+        <!-- main slider[end] -->
+        <div class="container">
+            <div class="prod_posts_each py-3"> <!-- itration -->
+                <div class="row align-items-center">
+                    <div class="col-md-7">
+                        <div class="c_title">
+                            <h2 class="c_title--inner lined">Agriculture</h2>
+                        </div>
+                        <div class="part-top">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Radial Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+
+                        <div class="part-bottom">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Bias Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-5 pt-4 pt-md-0">
+                        <img src="http://localhost/ceat-spl/assets/images/prod-use-1.jpg" class="img-fluid" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="prod_posts_each py-3 r_xs--rev"> <!-- itration with reverse -->
+                <div class="row align-items-center">
+                    <div class="col-md-7">
+                        <div class="c_title">
+                            <h2 class="c_title--inner lined">Agriculture</h2>
+                        </div>
+                        <div class="part-top">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Radial Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+
+                        <div class="part-bottom">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Bias Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-5 pt-4 pt-md-0">
+                        <img src="http://localhost/ceat-spl/assets/images/prod-use-1.jpg" class="img-fluid" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="prod_posts_each py-3"> <!-- itration with video player-->
+                <div class="row align-items-center">
+                    <div class="col-md-7">
+                        <div class="c_title">
+                            <h2 class="c_title--inner lined">Agriculture</h2>
+                        </div>
+                        <div class="part-top">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Radial Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+
+                        <div class="part-bottom">
+                            <div class="py-3"></div>
+                            <p>The Radial Range</p>
+                            <p>The radial tyres are designed to ensure you get the Power of C-T-R, i.e. Low Compaction, High Traction and High Roadability. Aimed to give you an all-round performance and peace of mind. Due to their flexible sidewalls, superior construction, higher tread arc width &amp; higher inner tyre volume, our radial tyres help reduce soil compaction. Their dual lug angle design with more lug overlap gives it superior traction and better roadability.</p>
+                            <div class="py-2"></div>
+                            <a href="javascript:void(0)" class="prod_posts_each--btn">View Bias Tyres
+                                <svg width="3.753" height="6.7" viewBox="0 0 3.753 6.7">
+                                    <g transform="translate(38.597 29.637) rotate(180)">
+                                        <g transform="translate(34.844 22.937)">
+                                            <path d="M34.844,978.65l.244.255,2.971,3.1.538-.511-2.727-2.84,2.727-2.84-.538-.511-2.971,3.1Z" transform="translate(-34.844 -975.3)"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-5 pt-4 pt-md-0">
+                        <div class="prod_posts_each_video v-pop" data-type="youtube" data-id="udnjPDsUBug" data-autoplay="true">
+                            <img src="http://localhost/ceat-spl/assets/images/prod-use-1.jpg" class="img-fluid" alt="image">
+                            <div class="prod_posts_each_video--btn text-center">
+                                <svg viewBox="0 0 55 55" style="enable-background:new 0 0 55 55;">
+                                    <g>
+                                        <path fill="#076ED5" d="M27.5,0C42.7,0,55,12.3,55,27.5S42.7,55,27.5,55S0,42.7,0,27.5S12.3,0,27.5,0z"></path>
+                                        <path fill="#F6FAFB" d="M25.1,20.1v14.4l8.5-7.2L25.1,20.1z"></path>
+                                    </g>
+                                </svg>
+                            </div>
+                            <p class="prod_posts_each_video--text text-center">Watch video</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- website footer[start] -->
+    <?php include('../components/shared/footer.php') ?>
+    <!-- website footer[end] -->
+    <!-- additonal popups[start] -->
+    <?php include('../components/video-popup.php') ?>
+    <!-- additonal popups[end] -->
+    <!-- scripts[start] -->
+    <?php include('../includes/bottom-scripts.php') ?> 
+    <!-- scripts[end] -->
+    <!-- page dependancy scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
+    <!-- page scripts -->
+    <script src="<?php echo $assetsurl ?>js/page-js/index.js"></script>
+</body>
+</html>
