49e4f97ac4c33f1264ea383de741b2eebfbe7d93
gokul <gokul@22feettribalww.com>
2019-04-15 23:01:58 +0530

    life ceat - wip

---
+++ pages/life-at-ceat.php
@@ -0,0 +1,160 @@
+<?php include("../includes/settings.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page</title>
+    <?php include('../includes/header-imports.php') ?>
+</head>
+<body>
+    <!-- website header[start] -->
+    <?php include('../components/shared/header.php') ?> 
+    <!-- website header[end] -->
+    <!-- common loader[start] -->
+    <?php include('../components/shared/loader.php') ?>
+    <!-- common loader[end] -->
+    <main>
+        <!-- cremb[start] -->
+        <?php include('../components/page-header-and-cremb-no-bg.php') ?>
+        <!-- cremb[end] -->
+        <!-- banner image type[start] -->
+        <div class="image_banner">
+            <img src="" data-m="<?php echo $imagesurl?>life/banner-mob.jpg" data-d="<?php echo $imagesurl?>life/banner.jpg" class="img-fluid w-100 multi-img" alt="banner">
+        </div>
+        <!-- banner image type[end] -->
+        <!-- tab secs[start] -->
+        <section class="tab_wraps py-5">
+            <div class="tab_wraps_each py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="about_info_btns">
+                                <div class="about_info_btns--each state-active">
+                                    <span>manufacturing facilities</span>
+                                </div>
+                                <div class="about_info_btns--each">
+                                    <span>Technology</span>
+                                </div>
+                                <div class="about_info_btns--each">
+                                    <span>Market</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="tab_wraps_each_post">
+                                <p class="c_text--inner">At CEAT, we strive to create a work culture which stimulates employees&#39; professional growth . The Employee-Management relationship is built on pillars like trust, respect and integrity. Such an environment is conducive to bringing out the best of their creative and analytical abilities through hands-on-deck experience.</p>
+                            </div>
+                            <img src="<?php echo $imagesurl ?>life/post/1.jpg" class="img-fluid w-100" alt="img">
+                            <div class="py-3"></div>
+                            <h4 class="c_title--inner small lined pb-3">Quality Based Management</h4>
+                            <p class="c_text--inner">QBM is followed at CEAT as a way of management for the entire organization. It consists of philosophy, systems, tools and technique from Total Quality Management (TQM), Total Productive Maintenance (TPM) and Toyota Production System (TPS).</p>
+                            <div class="t_care_each_data_listing">
+                                <h5 class="t_care_each_data_listing--title">So be sure to give them both a daily visual inspection. Watch the video for the basics on tractor and tire care:</h5>
+                                <ul>
+                                    <li>What to look out for in your daily inspections.</li>
+                                    <li>Tips on load carrying.</li>
+                                    <li>How often to check oils, fluid, water and batteries.</li>
+                                    <li>How to keep your tractor and its parts clean.</li>
+                                    <li>Preventive tips to prolong the life of your tractor and tires. </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab_wraps_each py-5 bg_light">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6"></div>
+                        <div class="col-md-6">
+                            <h2 class="tab_wraps_each--title py-4">CAIRO</h2>
+                            <!-- 1 -->
+                            <div class="tab_wraps_each_high">
+                                <div class="d-flex">
+                                    <div class="tab_wraps_each_high--letter">C</div>
+                                    <div class="tab_wraps_each_high--copy">
+                                        <h4 class="c_title--inner">Challenger</h4>
+                                        <p class="c_text--inner">Innovative and Agile, Questioning existing ways, <br/>Promoting Experimentation</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="t_care_each_data_listing mb-5">
+                                <h5 class="t_care_each_data_listing--title">So be sure to give them both a daily visual inspection. Watch the video for the basics on tractor and tire care:</h5>
+                                <ul>
+                                    <li>What to look out for in your daily inspections.</li>
+                                    <li>Tips on load carrying.</li>
+                                    <li>How often to check oils, fluid, water and batteries.</li>
+                                    <li>How to keep your tractor and its parts clean.</li>
+                                    <li>Preventive tips to prolong the life of your tractor and tires. </li>
+                                </ul>
+                            </div>
+                            <!-- 2 -->
+                            <div class="tab_wraps_each_high">
+                                <div class="d-flex">
+                                    <div class="tab_wraps_each_high--letter">C</div>
+                                    <div class="tab_wraps_each_high--copy">
+                                        <h4 class="c_title--inner">Challenger</h4>
+                                        <p class="c_text--inner">Innovative and Agile, Questioning existing ways, <br/>Promoting Experimentation</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="t_care_each_data_listing mb-5">
+                                <h5 class="t_care_each_data_listing--title">So be sure to give them both a daily visual inspection. Watch the video for the basics on tractor and tire care:</h5>
+                                <ul>
+                                    <li>What to look out for in your daily inspections.</li>
+                                    <li>Tips on load carrying.</li>
+                                    <li>How often to check oils, fluid, water and batteries.</li>
+                                    <li>How to keep your tractor and its parts clean.</li>
+                                    <li>Preventive tips to prolong the life of your tractor and tires. </li>
+                                </ul>
+                            </div>
+                            <!-- 3 -->
+                            <div class="tab_wraps_each_high">
+                                <div class="d-flex">
+                                    <div class="tab_wraps_each_high--letter">C</div>
+                                    <div class="tab_wraps_each_high--copy">
+                                        <h4 class="c_title--inner">Challenger</h4>
+                                        <p class="c_text--inner">Innovative and Agile, Questioning existing ways, <br/>Promoting Experimentation</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="t_care_each_data_listing mb-5">
+                                <h5 class="t_care_each_data_listing--title">So be sure to give them both a daily visual inspection. Watch the video for the basics on tractor and tire care:</h5>
+                                <ul>
+                                    <li>What to look out for in your daily inspections.</li>
+                                    <li>Tips on load carrying.</li>
+                                    <li>How often to check oils, fluid, water and batteries.</li>
+                                    <li>How to keep your tractor and its parts clean.</li>
+                                    <li>Preventive tips to prolong the life of your tractor and tires. </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- tab secs[end] -->
+    </main>
+    <!-- website footer[start] -->
+    <?php include('../components/shared/footer.php') ?>
+    <!-- website footer[end] -->
+    <!-- additonal popups[start] -->
+    <?php include('../components/video-popup.php') ?>
+    <!-- additonal popups[end] -->
+    <!-- scripts[start] -->
+    <?php include('../includes/bottom-scripts.php') ?> 
+    <!-- scripts[end] -->
+    <!-- page dependancy scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sticky-kit/1.1.3/sticky-kit.min.js"></script>
+    <!-- page scripts -->
+    <script>
+        jQuery(document).ready(function($){
+            if($(window).width() > 767){
+                $(".about_info_btns").stick_in_parent({
+                    parent: ".tab_wraps",
+                    offset_top:150
+                });
+            }
+        }(jQuery))
+    </script>
+</body>
+</html>
