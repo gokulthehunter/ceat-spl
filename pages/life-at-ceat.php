294076822ef435e9da846eb1dea25aa7d48b8a7d
gokul <gokul@22feettribalww.com>
2019-04-18 12:01:35 +0530

    life ceat - wip

--- pages/life-at-ceat.php
+++ pages/life-at-ceat.php
@@ -131,6 +131,33 @@
                     </div>
                 </div>
             </div>
+            <div class="tab_wraps_each py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6"></div>
+                        <div class="col-md-6">
+                            <div class="tab_wraps_each_post">
+                            <h4 class="c_title--inner">Fun at Work Place</h4>
+                                <p class="c_text--inner">At CEAT, we strive to create a work culture which stimulates employees&#39; professional growth . The Employee-Management relationship is built on pillars like trust, respect and integrity. Such an environment is conducive to bringing out the best of their creative and analytical abilities through hands-on-deck experience.</p>
+                            </div>
+                            <img src="<?php echo $imagesurl ?>life/post/1.jpg" class="img-fluid w-100" alt="img">
+                            <div class="py-3"></div>
+                            <h4 class="c_title--inner small lined pb-3">Quality Based Management</h4>
+                            <p class="c_text--inner">QBM is followed at CEAT as a way of management for the entire organization. It consists of philosophy, systems, tools and technique from Total Quality Management (TQM), Total Productive Maintenance (TPM) and Toyota Production System (TPS).</p>
+                            <div class="t_care_each_data_listing">
+                                <h5 class="t_care_each_data_listing--title">So be sure to give them both a daily visual inspection. Watch the video for the basics on tractor and tire care:</h5>
+                                <ul>
+                                    <li>What to look out for in your daily inspections.</li>
+                                    <li>Tips on load carrying.</li>
+                                    <li>How often to check oils, fluid, water and batteries.</li>
+                                    <li>How to keep your tractor and its parts clean.</li>
+                                    <li>Preventive tips to prolong the life of your tractor and tires. </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
         <!-- tab secs[end] -->
     </main>
